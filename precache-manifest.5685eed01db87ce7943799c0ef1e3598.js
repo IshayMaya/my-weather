ed9284e8cb089b80657de0ccf93d0821f1df0f83
ishayMaya <ssaymama@gmail.com>
2019-08-21 18:47:33 +0300

    Updates

---
+++ precache-manifest.5685eed01db87ce7943799c0ef1e3598.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9102760d24daa56d03ad0506fa1e8d4",
+    "url": "/my-weather/index.html"
+  },
+  {
+    "revision": "39c84755836d0dbba9b6",
+    "url": "/my-weather/static/css/main.d7ae75bc.chunk.css"
+  },
+  {
+    "revision": "5bf069c7533b656bcf4f",
+    "url": "/my-weather/static/js/2.dc73f5a8.chunk.js"
+  },
+  {
+    "revision": "39c84755836d0dbba9b6",
+    "url": "/my-weather/static/js/main.9ce9762c.chunk.js"
+  },
+  {
+    "revision": "71373542fb7dfcf4e5f5",
+    "url": "/my-weather/static/js/runtime~main.37f04531.js"
+  },
+  {
+    "revision": "060ba68bc1be9ce9a3db669c4de9ba84",
+    "url": "/my-weather/static/media/forest3.060ba68b.jpg"
+  }
+]);
